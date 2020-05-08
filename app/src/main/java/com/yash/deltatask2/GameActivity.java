0d77b585f24053cac64d9f286dee255fb9699526
Yash Mohan <yashmohan2206@gmail.com>
2020-05-09 00:33:13 +0530

    same

--- app/src/main/java/com/yash/deltatask2/GameActivity.java
+++ app/src/main/java/com/yash/deltatask2/GameActivity.java
@@ -9,15 +9,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 public class GameActivity extends AppCompatActivity {
     public com.yash.deltatask2.lines L;
-    RelativeLayout rl,main_rl;
-    float x,y;
+    RelativeLayout rl, main_rl;
+    float x, y;
     MediaPlayer mp;
-    TextView player_1_win,player_2_win;
-    int gridHeight,gridWidth;
-    public static int count =1,number_of_players=2;
+    TextView player_1_win, player_2_win;
+    int gridHeight, gridWidth;
+    public static int count = 1, number_of_players = 2;
     public static int[][] horizontal_lines;
     public static int[][] vertical_lines;
     public static int[] players_win;
@@ -28,15 +30,15 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         Intent intent = getIntent();
-        count=0;
+        count = 0;
         sound = findViewById(R.id.soundgame);
-        gridHeight=intent.getIntExtra("grid_H",5);
-        gridWidth=intent.getIntExtra("grid_W",5);
-        if(intent.getIntExtra("number_of_players",2)>=2){
-            number_of_players=intent.getIntExtra("number_of_players",2);
+        gridHeight = intent.getIntExtra("grid_H", 5);
+        gridWidth = intent.getIntExtra("grid_W", 5);
+        if (intent.getIntExtra("number_of_players", 2) >= 2) {
+            number_of_players = intent.getIntExtra("number_of_players", 2);
         }
-        if(mp==null){
-            mp = MediaPlayer.create(this,R.raw.mainactivitymusic);
+        if (mp == null) {
+            mp = MediaPlayer.create(this, R.raw.mainactivitymusic);
             mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
@@ -45,21 +47,21 @@ public class GameActivity extends AppCompatActivity {
             });
         }
         mp.start();
-        players_win=new int[number_of_players];
-        main_rl=findViewById(R.id.relativeLayout);
-        horizontal_lines=new int[gridWidth-1][gridHeight];
-        vertical_lines=new int[gridHeight-1][gridWidth];
+        players_win = new int[number_of_players];
+        main_rl = findViewById(R.id.relativeLayout);
+        horizontal_lines = new int[gridWidth - 1][gridHeight];
+        vertical_lines = new int[gridHeight - 1][gridWidth];
         //boxes=new int[gridWidth-1][gridHeight-1];
-        player_1_win=findViewById(R.id.player_1_win);
-        player_2_win=findViewById(R.id.player_2_win);
-        dotsAndBoxes dab=new dotsAndBoxes(GameActivity.this,main_rl,gridHeight,gridWidth);
+        player_1_win = findViewById(R.id.player_1_win);
+        player_2_win = findViewById(R.id.player_2_win);
+        dotsAndBoxes dab = new dotsAndBoxes(GameActivity.this, main_rl, gridHeight, gridWidth);
         main_rl.addView(dab);
         main_rl.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                x=event.getX();
-                y=event.getY();
-                lines lines = new lines(GameActivity.this,main_rl,x,y,gridHeight,gridWidth);
+                x = event.getX();
+                y = event.getY();
+                lines lines = new lines(GameActivity.this, main_rl, x, y, gridHeight, gridWidth);
                 main_rl.addView(lines);
                 player_1_win.setText(String.valueOf(players_win[0]));
                 player_2_win.setText(String.valueOf(players_win[1]));
@@ -75,9 +77,8 @@ public class GameActivity extends AppCompatActivity {
             mp.stop();
             mp = null;
             sound.setBackgroundResource(R.drawable.ic_volume_off_black_24dp);
-        }
-        else{
-            mp = MediaPlayer.create(this,R.raw.mainactivitymusic);
+        } else {
+            mp = MediaPlayer.create(this, R.raw.mainactivitymusic);
             mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
@@ -88,6 +89,7 @@ public class GameActivity extends AppCompatActivity {
             sound.setBackgroundResource(R.drawable.ic_volume_up_black_24dp);
         }
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
--- app/src/main/java/com/yash/deltatask2/dotsAndBoxes.java
+++ app/src/main/java/com/yash/deltatask2/dotsAndBoxes.java
@@ -1,4 +1,5 @@
 package com.yash.deltatask2;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -6,26 +7,29 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
 import android.widget.RelativeLayout;
+
 @SuppressLint("ViewConstructor")
 public class dotsAndBoxes extends View {
     RelativeLayout rl;
-    Paint p,p_background;
-    int grid_width,grid_height;
-    public dotsAndBoxes(Context context, RelativeLayout relativeLayout,int gridHeight, int gridWidth) {
+    Paint p, p_background;
+    int grid_width, grid_height;
+
+    public dotsAndBoxes(Context context, RelativeLayout relativeLayout, int gridHeight, int gridWidth) {
         super(context);
-        rl=relativeLayout;
-        p=new Paint();
-        p_background=new Paint();
+        rl = relativeLayout;
+        p = new Paint();
+        p_background = new Paint();
         p.setColor(Color.BLACK);
-        grid_width=gridWidth-1;
-        grid_height=gridHeight-1;
+        grid_width = gridWidth - 1;
+        grid_height = gridHeight - 1;
         p_background.setColor(Color.YELLOW);
     }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        for(float i = 100;i<=rl.getWidth()-100;i+=(getWidth()-200)/(grid_width)) {
-            for (float j = getHeight()/4f; j < 3*getHeight()/4f; j += getHeight()/(2*(grid_height))) {
+        for (float i = 100; i <= rl.getWidth() - 100; i += (getWidth() - 200) / (grid_width)) {
+            for (float j = getHeight() / 4f; j < 3 * getHeight() / 4f; j += getHeight() / (2 * (grid_height))) {
                 canvas.drawCircle(i, j, 17.5f, p);
             }
         }
